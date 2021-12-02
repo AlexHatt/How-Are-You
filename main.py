b07753c38895f668cb21e85d29aa1fc34a10bbc6
@AlexHatt <94867315+AlexHatt@users.noreply.github.com>
2021-12-02 15:40:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+You_Rn = input("How are you today?\n")
+if You_Rn == "good":
+  print("Nice!")
+if You_Rn == "bad":
+  print("https://www.bbc.co.uk/iplayer/episodes/b07xdmgk/hey-duggee\n\nHope your day gets better.")
