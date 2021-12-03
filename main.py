4a520b35a62cc7f3343b12bd8f444c3ba5bb221a
@AlexHatt <94867315+AlexHatt@users.noreply.github.com>
2021-12-03 09:09:44 +0000

    Updated

--- main.py
+++ main.py
@@ -3,3 +3,24 @@ if You_Rn == "good":
   print("Nice!")
 if You_Rn == "bad":
   print("https://www.bbc.co.uk/iplayer/episodes/b07xdmgk/hey-duggee\n\nHope your day gets better.")
+if You_Rn == "sad":
+  print("You rn :(")
+if You_Rn == "happy":
+  print("Sounds good!")
+if You_Rn == "my dog died":
+  print("plagairism")
+if You_Rn == "ill":
+  print("get well soon")
+if You_Rn == "feline good":
+  print("nope\nleave\n\nleave please")
+  quit()
+if You_Rn == "enlightened":
+  print("good for you, i guess")
+if You_Rn == "jesus":
+  input("captain falcon? or ness?")
+if You_Rn == "":
+  print("bruh")
+if You_Rn == "pneunomonoultramicroscopicsilicovolcanoconiosis":
+  print("can't believe you spelt it right")
+if You_Rn == "No u":
+  print("hahahhahah")
